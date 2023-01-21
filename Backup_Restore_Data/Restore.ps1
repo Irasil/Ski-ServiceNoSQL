1c983901a39865adf676ca2f8fb89da0ca0bb42f
Irasil <91846030+Irasil@users.noreply.github.com>
2023-01-21 01:55:03 +0100

    Einfaches Backup

---
+++ Backup_Restore_Data/Restore.ps1
@@ -0,0 +1,3 @@
+$backupfile = "C:\Users\Administrator\OneDrive - ipso! Bildung\Schule\Modul 165\Projektarbeit\Backup\Ski_Orders_21.01.2023\Ski\Orders.bson"
+
+mongorestore.exe --db Ski --collection Orders $backupfile
