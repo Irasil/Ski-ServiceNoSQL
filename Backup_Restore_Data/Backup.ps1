7b9dffca877ce133274cc5968bc8407a466de0f8
Irasil <91846030+Irasil@users.noreply.github.com>
2023-02-04 22:00:50 +0100

    Dokumentation, Präsi und Skripts

--- Backup_Restore_Data/Backup.ps1
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-$date = Get-Date -Format "dd/MM/yyyy"
-
-
-
-# Create a backup of the Ski database and its Orders collection
-mongodump --db Ski --collection Orders --out "C:\Users\Administrator\OneDrive - ipso! Bildung\Schule\Modul 165\Projektarbeit\Backup\Ski_Orders_$date"
-
--- Backup_Restore_Data/Restore.ps1
+++
@@ -1,3 +0,0 @@
-$backupfile = "C:\Users\Administrator\OneDrive - ipso! Bildung\Schule\Modul 165\Projektarbeit\Backup\Ski_Orders_21.01.2023\Ski\Orders.bson"
-
-mongorestore.exe --db Ski --collection Orders $backupfile
